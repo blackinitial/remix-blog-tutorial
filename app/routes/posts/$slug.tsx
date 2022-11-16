adf99b674ac99e5a32ebc0cefb1e12b66788417a
blackinitial <alhanif.blackinitial@gmail.com>
2022-11-16 19:11:56 +0700

    📃 convert markdown to HTML on server

--- app/routes/posts/$slug.tsx
+++ app/routes/posts/$slug.tsx
@@ -1,18 +1,21 @@
-import { LoaderFunction, json } from "@remix-run/node"
+import {marked} from 'marked';
+import { LoaderFunction, json } from "@remix-run/node";
 import { useLoaderData } from "@remix-run/react";
 import { getPost } from "~/models/post.server";
 
 export const loader: LoaderFunction = async ({params}) => {
-  const {slug} = params
-  const post = await getPost(slug)
-  return json({ post });
+  const {slug} = params;
+  const post = await getPost(slug);
+  const html = marked(post.markdown);
+  return json({ title: post.title, html });
 };
 
 export default function PostRoute() {
-  const { post } = useLoaderData();
+  const { title, html } = useLoaderData();
   return (
     <main className="mx-auto max-w-4xl">
-      <h1 className="my-6 border-b-2 text-center text-3xl">{post.title}</h1>
+      <h1 className="my-6 border-b-2 text-center text-3xl">{title}</h1>
+      <div dangerouslySetInnerHTML={{__html: html}} />
     </main>
   );
 }
