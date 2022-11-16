d398dcd667cfa285cf430423f2d6aed30ac7a3f0
blackinitial <alhanif.blackinitial@gmail.com>
2022-11-16 17:30:50 +0700

    📀 simple fetch data

--- app/routes/posts/index.tsx
+++ app/routes/posts/index.tsx
@@ -1,7 +1,36 @@
+import { json } from '@remix-run/node';
+import { useLoaderData, Link } from '@remix-run/react';
+
+export const loader = async () => {
+  const posts = [
+    {
+      slug: "my-first-post",
+      title: "My First Post"
+    },
+    {
+      slug: "second-post-ready",
+      title: "Second Post Ready"
+    }
+  ];
+
+  return json({posts});
+}
+
 export default function PostsRoute() {
+  const { posts } = useLoaderData();
+  
   return (
     <main>
       <h1>Posts</h1>
+      <ul>
+        {posts.map((post) => (
+          <li key={post.slug}>
+            <Link to={post.slug} className="text-blue-600 underline">
+              {post.title}
+            </Link>
+          </li>
+        ))}
+      </ul>
     </main>
   )
 }
