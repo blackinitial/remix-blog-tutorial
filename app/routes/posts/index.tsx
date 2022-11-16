8dffa09192fe867ed9c4ccbfc3ff61f47bc098f8
blackinitial <alhanif.blackinitial@gmail.com>
2022-11-16 17:43:45 +0700

    ⛑ Integrate safe type Typescript on loader func

--- app/routes/posts/index.tsx
+++ app/routes/posts/index.tsx
@@ -1,14 +1,19 @@
 import { json } from '@remix-run/node';
+import type { LoaderFunction } from '@remix-run/node';
 import { useLoaderData, Link } from '@remix-run/react';
 import { getPosts } from '~/models/post.server';
 
-export const loader = async () => {
+type LoaderData = {
+  posts: Awaited<ReturnType<typeof getPosts>>
+}
+
+export const loader: LoaderFunction = async () => {
   const posts = await getPosts();
-  return json({posts});
+  return json<LoaderData>({posts});
 }
 
 export default function PostsRoute() {
-  const { posts } = useLoaderData();
+  const { posts } = useLoaderData() as LoaderData;
   
   return (
     <main>
