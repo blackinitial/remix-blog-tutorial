5b12a8f1861aa0d1ad238b2b994d3120dca931ce
blackinitial <alhanif.blackinitial@gmail.com>
2022-11-16 18:56:46 +0700

    ⬆️ made dynamic route to get single dynamic post

--- app/models/post.server.ts
+++ app/models/post.server.ts
@@ -1,5 +1,18 @@
 import { prisma } from "~/db.server";
 
+export async function getPostLIstings() {
+  return prisma.post.findMany({
+    select: {
+      slug: true,
+      title: true
+    }
+  })
+}
+
 export async function getPosts() {
   return prisma.post.findMany();
+}
+
+export async function getPost(slug: string) {
+  return prisma.post.findUnique({ where: { slug } });
 }
---
+++ app/routes/posts/$slug.tsx
@@ -0,0 +1,18 @@
+import { LoaderFunction, json } from "@remix-run/node"
+import { useLoaderData } from "@remix-run/react";
+import { getPost } from "~/models/post.server";
+
+export const loader: LoaderFunction = async ({params}) => {
+  const {slug} = params
+  const post = await getPost(slug)
+  return json({ post });
+};
+
+export default function PostRoute() {
+  const { post } = useLoaderData();
+  return (
+    <main className="mx-auto max-w-4xl">
+      <h1 className="my-6 border-b-2 text-center text-3xl">{post.title}</h1>
+    </main>
+  );
+}
