f108ee153aab4099c6bc98e796be0ad45fdf9512
blackinitial <alhanif.blackinitial@gmail.com>
2022-11-16 19:55:06 +0700

    ⛑ TS cover for dynamic parameter with invariant

--- app/routes/posts/$slug.tsx
+++ app/routes/posts/$slug.tsx
@@ -1,17 +1,28 @@
 import {marked} from 'marked';
-import { LoaderFunction, json } from "@remix-run/node";
+import type { LoaderFunction } from '@remix-run/node';
+import { json } from "@remix-run/node";
 import { useLoaderData } from "@remix-run/react";
 import { getPost } from "~/models/post.server";
+import invariant from 'tiny-invariant';
+
+type LoaderData = {
+  title: string,
+  html: string
+}
 
 export const loader: LoaderFunction = async ({params}) => {
   const {slug} = params;
+  invariant(slug, 'slug is required');
+
   const post = await getPost(slug);
+  invariant(post, 'post not found');
+
   const html = marked(post.markdown);
-  return json({ title: post.title, html });
+  return json<LoaderData>({ title: post.title, html });
 };
 
 export default function PostRoute() {
-  const { title, html } = useLoaderData();
+  const { title, html } = useLoaderData() as LoaderData;
   return (
     <main className="mx-auto max-w-4xl">
       <h1 className="my-6 border-b-2 text-center text-3xl">{title}</h1>
