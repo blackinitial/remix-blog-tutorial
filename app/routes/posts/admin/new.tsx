988bea1ccf1b3914b235d2685db8676f5e409809
blackinitial <alhanif.blackinitial@gmail.com>
2022-11-17 17:37:29 +0700

    ✉️ handle form POST with remix actions

--- app/routes/posts/admin/new.tsx
+++ app/routes/posts/admin/new.tsx
@@ -1,3 +1,48 @@
+import { Form } from "@remix-run/react";
+import { redirect } from "@remix-run/node";
+import type { ActionFunction } from "@remix-run/node";
+import { createPost } from "~/models/post.server";
+
+export const action: ActionFunction = async ({request}) => {
+  const formData = await request.formData();
+
+  const title = formData.get('title');
+  const slug = formData.get('slug')
+  const markdown = formData.get('markdown')
+
+  await createPost({title, slug, markdown})
+  
+  return redirect("/posts/admin");
+}
+
+const inputClassName = `w-full rounded border border-gray-500 px-2 py-1 text-lg`;
+
 export default function NewPostRoute() {
-  return <h2>New Post</h2>;
+  return (
+    <Form method="post">
+      <p>
+        <label htmlFor="title">
+          Post Title:
+          <input type="text" name="title" className={inputClassName} />
+        </label>
+      </p>
+      <p>
+        <label htmlFor="slug">
+          Post Slug:
+          <input type="text" name="slug" className={inputClassName} />
+        </label>
+      </p>
+      <p>
+        <label htmlFor="markdown">
+          Markdown:
+          <textarea name="markdown" id="markdowon" rows="20" className={`${inputClassName} font-mono`} />
+        </label>
+      </p>
+      <p className="text-right">
+        <button type="submit" className="rounded bg-blue-500 px-4 text-white">
+          Create Post
+        </button>
+      </p>
+    </Form>
+  )
 }
